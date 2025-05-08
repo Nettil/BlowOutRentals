6ac105d9d359dd4a85860c89e873224de21b0b1b
Nettil <NathanLX10@Gmail.com>
2025-05-08 03:15:08 -0500

    Added new apps to app definition in settings.py

--- BlowOutRentals/settings.py
+++ BlowOutRentals/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'homepages.apps.HomepagesConfig',
+    'inventory.apps.InventoryConfig',
 ]
 
 MIDDLEWARE = [
